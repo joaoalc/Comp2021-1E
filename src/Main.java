3d90aaa07172504c7da07c8f563494dd319020de
Educorreia932 <skelozard1@gmail.com>
2021-03-07 22:57:57 +0000

    Started to convert the e-BNF grammar into a JavaCC grammar

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,20 @@ import java.util.ArrayList;
 import java.io.StringReader;
 
 public class Main implements JmmParser {
+    public JmmParserResult parse(String jmmCode) {
 
+        try {
+            Calculator myCalc = new Calculator(new StringReader(jmmCode));
+            SimpleNode root = myCalc.Expression(); // returns reference to root node
 
-	public JmmParserResult parse(String jmmCode) {
-		
-		try {
-		    Calculator myCalc = new Calculator(new StringReader(jmmCode));
-    		SimpleNode root = myCalc.Expression(); // returns reference to root node
-            	
-    		root.dump(""); // prints the tree on the screen
-    	
-    		return new JmmParserResult(root, new ArrayList<Report>());
-		} catch(ParseException e) {
-			throw new RuntimeException("Error while parsing", e);
-		}
-	}
+            root.dump(""); // prints the tree on the screen
+
+            return new JmmParserResult(root, new ArrayList<Report>());
+        }
+        catch (ParseException e) {
+            throw new RuntimeException("Error while parsing", e);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Executing with args: " + Arrays.toString(args));
