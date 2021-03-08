57767ee18de226fc1eab5ab7b48695fcabdf8836
Educorreia932 <skelozard1@gmail.com>
2021-03-08 20:21:11 +0000

    Tried to remove left recursion of grammar

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,38 @@ import java.io.StringReader;
 
 public class Main implements JmmParser {
     public JmmParserResult parse(String jmmCode) {
-
         try {
-            Calculator myCalc = new Calculator(new StringReader(jmmCode));
-            SimpleNode root = myCalc.Expression(); // returns reference to root node
+            JMM jmm = new JMM(new StringReader(jmmCode));
+            SimpleNode root = jmm.Program(); // returns reference to root node
 
             root.dump(""); // prints the tree on the screen
 
             return new JmmParserResult(root, new ArrayList<Report>());
         }
+
         catch (ParseException e) {
             throw new RuntimeException("Error while parsing", e);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Executing with args: " + Arrays.toString(args));
-        if (args[0].contains("fail")) {
-            throw new RuntimeException("It's supposed to fail");
-        }
+        String code = "import io;\n" +
+        "class Fac {\n" +
+        " public int ComputeFac(int num){\n" +
+        " int num_aux ;\n" +
+        " if (num < 1)\n" +
+        " num_aux = 1;\n" +
+        " else\n" +
+        " num_aux = num * (this.ComputeFac(num-1));\n" +
+        " return num_aux;\n" +
+        " }\n" +
+        " public static void main(String[] args){\n" +
+        " io.println(new Fac().ComputeFac(10)); //assuming the existence\n" +
+        " // of the classfile io.class\n" +
+        " }\n" +
+        "}";
+
+        Main main = new Main();
+        main.parse(code);
     }
-
-
 }
