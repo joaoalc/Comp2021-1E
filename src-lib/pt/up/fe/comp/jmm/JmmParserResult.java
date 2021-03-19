582bd81ad60c8f0f4ae4236c810164546bf22dbf
João Bispo <joaobispo@gmail.com>
2021-03-19 00:05:13 +0000

    Adds JmmParserResult.toJson()

--- src-lib/pt/up/fe/comp/jmm/JmmParserResult.java
+++ src-lib/pt/up/fe/comp/jmm/JmmParserResult.java
@@ -1,10 +1,14 @@
 package pt.up.fe.comp.jmm;
 
-import pt.up.fe.comp.jmm.report.Report;
-
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import pt.up.fe.comp.jmm.report.Report;
+
 public class JmmParserResult {
+
     private final JmmNode rootNode;
     private final List<Report> reports;
 
@@ -12,13 +16,20 @@ public class JmmParserResult {
         this.rootNode = rootNode;
         this.reports = reports;
     }
-	
-	public JmmNode getRootNode() {
-		return this.rootNode;
-	}
-	
-	public List<Report> getReports() {
-		return this.reports;
-	}	
-}
 
+    public JmmNode getRootNode() {
+        return this.rootNode;
+    }
+
+    public List<Report> getReports() {
+        return this.reports;
+    }
+
+    public String toJson() {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(JmmNode.class, new JmmSerializer())
+                .create();
+        return gson.toJson(this, JmmParserResult.class);
+    }
+}
