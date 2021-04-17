25a776b467ce5f0eaebd40344b58eee274041474
Educorreia932 <up201806433@fe.up.pt>
2021-04-17 23:58:58 +0100

    Created an OpVerifierVisitor

--- src/AnalysisStage.java
+++ src/AnalysisStage.java
@@ -6,19 +6,14 @@ import pt.up.fe.comp.jmm.JmmNode;
 import pt.up.fe.comp.jmm.JmmParserResult;
 import pt.up.fe.comp.jmm.analysis.JmmAnalysis;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
-import pt.up.fe.comp.jmm.ast.examples.ExamplePostorderVisitor;
-import pt.up.fe.comp.jmm.ast.examples.ExamplePreorderVisitor;
-import pt.up.fe.comp.jmm.ast.examples.ExamplePrintVariables;
-import pt.up.fe.comp.jmm.ast.examples.ExampleVisitor;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp.jmm.report.Stage;
+import visitor.OpVerifierVisitor;
 
 public class AnalysisStage implements JmmAnalysis {
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult parserResult) {
-        List<Report> reports = new ArrayList<>();
-
         if (TestUtils.getNumReports(parserResult.getReports(), ReportType.ERROR) > 0) {
             var errorReport = new Report(ReportType.ERROR, Stage.SEMANTIC, -1,
                     "Started semantic analysis but there are errors from previous stage");
@@ -33,28 +28,12 @@ public class AnalysisStage implements JmmAnalysis {
 
         JmmNode node = parserResult.getRootNode();
 
-        System.out.println("Dump tree with Visitor where you control tree traversal");
-        ExampleVisitor visitor = new ExampleVisitor("Identifier", "id");
-        System.out.println(visitor.visit(node, ""));
+        List<Report> reports = new ArrayList<>();
 
-        System.out.println("Dump tree with Visitor that automatically performs preorder tree traversal");
-        var preOrderVisitor = new ExamplePreorderVisitor("Identifier", "id");
-        System.out.println(preOrderVisitor.visit(node, ""));
-
-        System.out.println(
-                "Create histogram of node kinds with Visitor that automatically performs postorder tree traversal");
-        var postOrderVisitor = new ExamplePostorderVisitor();
-        var kindCount = new HashMap<String, Integer>();
-        postOrderVisitor.visit(node, kindCount);
-        System.out.println("Kinds count: " + kindCount + "\n");
-
-        System.out.println(
-                "Print variables name and line, and their corresponding parent with Visitor that automatically performs preorder tree traversal");
-        var varPrinter = new ExamplePrintVariables("Variable", "name", "line");
-        varPrinter.visit(node, null);
+        OpVerifierVisitor opVerifierVisitor = new OpVerifierVisitor();
+        opVerifierVisitor.visit(node, reports);
 
         // No Symbol Table being calculated yet
         return new JmmSemanticsResult(parserResult, null, reports);
     }
-
 }
---
+++ src/visitor/OpVerifierVisitor.java
@@ -0,0 +1,36 @@
+package visitor;
+
+import pt.up.fe.comp.jmm.JmmNode;
+import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
+import pt.up.fe.comp.jmm.report.Report;
+
+import java.util.List;
+
+public class OpVerifierVisitor extends PreorderJmmVisitor<List<Report>, Boolean> {
+    public OpVerifierVisitor() {
+        addVisit("Operation", this::visitOp);
+    }
+
+    private boolean visitOp(JmmNode node, List<Report> reports) {
+        switch (node.get("op")) {
+            case "PLUS":
+                verifyInt(node, reports);
+                break;
+
+            default:
+                System.out.println("Not implemented yet");
+                break;
+        }
+
+        return true;
+    }
+
+    private void verifyInt(JmmNode binaryOperation, List<Report> reports) {
+        List<JmmNode> children = binaryOperation.getChildren();
+        JmmNode left = children.get(0);
+        JmmNode right = children.get(1);
+
+        System.out.println("CHILD LEFT:" + left);
+        System.out.println("CHILD RIGHT:" + right);
+    }
+}
