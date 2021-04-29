1d6c13cf86a66836f8d8925e65d3188f81efe7bd
Educorreia932 <up201806433@fe.up.pt>
2021-04-29 19:15:37 +0100

    Started to work on Jasmin generation

--- src/stage/BackendStage.java
+++ src/stage/BackendStage.java
@@ -2,10 +2,10 @@ package stage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import org.specs.comp.ollir.ClassUnit;
-import org.specs.comp.ollir.OllirErrorException;
+import org.specs.comp.ollir.*;
 
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
@@ -17,30 +17,61 @@ import pt.up.fe.specs.util.SpecsIo;
 
 /**
  * Copyright 2021 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
  */
 
 public class BackendStage implements JasminBackend {
-
     @Override
     public JasminResult toJasmin(OllirResult ollirResult) {
         ClassUnit ollirClass = ollirResult.getOllirClass();
 
         try {
             // Example of what you can do with the OLLIR class
-            ollirClass.checkMethodLabels(); // check the use of labels in the OLLIR loaded
-            ollirClass.buildCFGs(); // build the CFG of each method
-            ollirClass.outputCFGs(); // output to .dot files the CFGs, one per method
-            ollirClass.buildVarTables(); // build the table of variables for each method
-            ollirClass.show(); // print to console main information about the input OLLIR
+
+            ollirClass.checkMethodLabels();  // check the use of labels in the OLLIR loaded
+            ollirClass.buildCFGs();          // build the CFG of each method
+            ollirClass.outputCFGs();         // output to .dot files the CFGs, one per method
+            ollirClass.buildVarTables();     // build the table of variables for each method
+            ollirClass.show();               // print to console main information about the input OLLIR
+
+            for (Method method : ollirClass.getMethods()) {
+                System.out.println("Method name: " + method.getMethodName());
+
+                for (Instruction instruction : method.getInstructions()) {
+                    System.out.println("Instruction: " + instruction.getInstType());
+
+                    switch (instruction.getInstType()) {
+                        case CALL:
+                            break;
+                        case GOTO:
+                            break;
+                        case NOPER:
+                            break;
+                        case ASSIGN:
+                            break;
+                        case BRANCH:
+                            break;
+                        case RETURN:
+                            break;
+                        case GETFIELD:
+                            break;
+                        case PUTFIELD:
+                            break;
+                        case UNARYOPER:
+                            break;
+                        case BINARYOPER:
+                            break;
+                    }
+                }
+            }
 
             // Convert the OLLIR to a String containing the equivalent Jasmin code
             String jasminCode = ""; // Convert node ...
@@ -49,12 +80,26 @@ public class BackendStage implements JasminBackend {
             List<Report> reports = new ArrayList<>();
 
             return new JasminResult(ollirResult, jasminCode, reports);
-
-        } catch (OllirErrorException e) {
-            return new JasminResult(ollirClass.getClassName(), null,
-                    Arrays.asList(Report.newError(Stage.GENERATION, -1, -1, "Exception during Jasmin generation", e)));
         }
 
+        catch (OllirErrorException e) {
+            return new JasminResult(
+                    ollirClass.getClassName(),
+                    null,
+                    Collections.singletonList(
+                            Report.newError(
+                                    Stage.GENERATION,
+                                    -1,
+                                    -1,
+                                    "Exception during Jasmin generation",
+                                    e
+                            )
+                    )
+            );
+        }
     }
 
+    private void generate(AssignInstruction instruction) {
+
+    }
 }
--- test/BackendTest.java
+++ test/BackendTest.java
@@ -1,12 +1,12 @@
 
 /**
  * Copyright 2021 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -17,16 +17,28 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import pt.up.fe.comp.TestUtils;
+import pt.up.fe.comp.jmm.jasmin.JasminResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.ollir.OllirUtils;
 import pt.up.fe.specs.util.SpecsIo;
 
-public class BackendTest {
+import java.util.ArrayList;
 
+public class BackendTest {
     @Test
     public void testHelloWorld() {
-        var result = TestUtils.backend(SpecsIo.getResource("fixtures/public/HelloWorld.jmm"));
-        TestUtils.noErrors(result.getReports());
+//        JasminResult result = TestUtils.backend(SpecsIo.getResource("fixtures/public/HelloWorld.jmm"));
+//        TestUtils.noErrors(result.getReports());
 
-        var output = result.run();
+        OllirResult ollirResult = new OllirResult(
+                OllirUtils.parse(SpecsIo.getResource("fixtures/public/ollir/Fac.ollir")),
+                null,
+                new ArrayList<>()
+        );
+
+        JasminResult result = TestUtils.backend(ollirResult);
+
+        String output = result.run();
         assertEquals("Hello, World!", output.trim());
     }
 }
